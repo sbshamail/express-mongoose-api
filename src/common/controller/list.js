18498abd142de9ee7eb3a7c206a93a7a2088db96
crownintldev <crownintltechnology@gmail.com>
2024-01-23 16:19:57 +0500

    Bump version to $VERSION

--- src/common/controller/list.js
+++ src/common/controller/list.js
@@ -1,6 +1,6 @@
 const constants = require("../helpers/constants");
 const { Response } = require("../helpers/responseHandler");
-const {createAggregationPipeline} = require("./aggregation")
+const { createAggregationPipeline } = require("./aggregation");
 
 exports.listCommonAggregationFilterize = async (
   req,
@@ -10,7 +10,7 @@ exports.listCommonAggregationFilterize = async (
   customParams
 ) => {
   try {
-    const { searchTerm, sortField, columnFilters,showRemove } = req.query;
+    const { searchTerm, sortField, columnFilters, showRemove } = req.query;
     let sortOrder = req.query?.sortOrder ? parseInt(req.query?.sortOrder) : -1;
     let columnFiltersArray = [];
     if (columnFilters) {
@@ -27,7 +27,7 @@ exports.listCommonAggregationFilterize = async (
       sortOrder: sortOrder ? sortOrder : 1,
       columnFilters: columnFiltersArray,
       showRemove,
-      customParams
+      customParams,
     });
     // @ts-ignore
     const result = await model.aggregate(pipeline);
@@ -42,6 +42,46 @@ exports.listCommonAggregationFilterize = async (
   }
 };
 
+exports.listAggregation = async (
+  req,
+  res,
+  model,
+  createAggregationPipeline,
+  customParams
+) => {
+  try {
+    const { searchTerm, sortField, columnFilters, showRemove } = req.query;
+    let sortOrder = req.query?.sortOrder ? parseInt(req.query?.sortOrder) : -1;
+    let columnFiltersArray = [];
+    if (columnFilters) {
+      columnFiltersArray = JSON.parse(columnFilters);
+    }
+    let limit = req.query?.limit ? parseInt(req.query?.limit) : 20;
+    let page = req.query?.pageNumber ? parseInt(req.query?.pageNumber) : 1;
+    let skip = (page - 1) * limit;
+    const pipeline = createAggregationPipeline({
+      skip,
+      limit,
+      searchTerm,
+      sortField: sortField ? sortField : "createdAt",
+      sortOrder: sortOrder ? sortOrder : 1,
+      columnFilters: columnFiltersArray,
+      showRemove,
+      customParams,
+    });
+    // @ts-ignore
+    const result = await model.aggregate(pipeline);
+
+    const total = result.length > 0 ? result[0].total : 0;
+    const data = result.length > 0 ? result[0].data : [];
+
+    return { total, data };
+  } catch (error) {
+    console.log(model.modelName, error);
+    Response(res, 400, constants.GET_ERROR);
+  }
+};
+
 /**
  * Perform a bulk write operation for a file.
  * @param {Object} options - Options for the bulk write operation.
@@ -49,23 +89,27 @@ exports.listCommonAggregationFilterize = async (
  * @param {Object} options.customParams - The data to set in the update.
  * @param {Object} options.ids
  * @param {Function} [options.ownPipeline] - Optional MongoDB session.
- * @returns {Promise<Object>} 
+ * @returns {Promise<Object>}
  */
 
-exports.aggregationByIds=async({model,ids,customParams,ownPipeline})=>{
-  const document = ids && ids?.length? ids : [ids];
+exports.aggregationByIds = async ({
+  model,
+  ids,
+  customParams,
+  ownPipeline,
+}) => {
+  const document = ids && ids?.length ? ids : [ids];
   let pipeline;
-    if(customParams){
-          // @ts-ignore
-        pipeline = createAggregationPipeline({ ids: document,customParams});
-    }
-    else if(ownPipeline){
-         // @ts-ignore
-        pipeline = ownPipeline({ ids: document});
-    }
-  
+  if (customParams) {
+    // @ts-ignore
+    pipeline = createAggregationPipeline({ ids: document, customParams });
+  } else if (ownPipeline) {
+    // @ts-ignore
+    pipeline = ownPipeline({ ids: document });
+  }
+
   // @ts-ignore
   const aggregateResult = await model.aggregate(pipeline);
   const response = aggregateResult.length > 0 ? aggregateResult[0].data : [];
   return response;
-}
+};
