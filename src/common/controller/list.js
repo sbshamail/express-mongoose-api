27d3a79a589aff562b2ade2fbc370ced17583ca9
hamai <mhamail1223@gmail.com>
2024-02-01 09:50:04 +0500

    Bump version to $VERSION

--- src/common/controller/list.js
+++ src/common/controller/list.js
@@ -10,7 +10,7 @@ exports.listCommonAggregationFilterize = async (
   customParams
 ) => {
   try {
-    const { searchTerm, sortField, columnFilters, showRemove } = req.query;
+    const { searchTerm, sortField, columnFilters, deleted } = req.query;
     let sortOrder = req.query?.sortOrder ? parseInt(req.query?.sortOrder) : -1;
     let columnFiltersArray = [];
     if (columnFilters) {
@@ -26,7 +26,7 @@ exports.listCommonAggregationFilterize = async (
       sortField: sortField ? sortField : "createdAt",
       sortOrder: sortOrder ? sortOrder : 1,
       columnFilters: columnFiltersArray,
-      showRemove,
+      deleted,
       customParams,
     });
     // @ts-ignore
